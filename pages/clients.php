821b545212d1dd7001cecfac7b1d58c3aad92c33
dounia-elg <dounia.elg20@gmail.com>
2024-12-13 15:32:18 +0100

    Add navbar to the clients page

--- pages/clients.php
+++ pages/clients.php
@@ -5,22 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clients</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="../css/style-clients.css">
+    
 </head>
-<body class="bg-gray-100">
+<body class="bg-black">
 
-    <div class="container mx-auto my-10 px-4">
-
+    <!-----------------Header----------------------->
+    <nav class="header">
+        <div class="logo ">
+            <img src="../img/logooo.png" alt="car Logo">
+        </div>
+        <div class="nav-menu">
+            <ul>
+                <li><a href="../home.html" >Home</a></li>
+                <li><a href="../pages/clients.php" class="active">Clients</a></li>
+                <li><a href="../pages/cars.php">Cars</a></li>
+                <li><a href="../pages/contrats.php">Contrats</a></li>
+            </ul>
+        </div>
+    </nav>
+    
+    <div class="container mx-auto mt-[150px] px-20">
         <!---------- Header Section ---------->
         <div class="flex justify-between items-center mb-6">
-            <h2 class="text-2xl font-semibold text-gray-800">All Clients</h2>
-            <a class="bg-green-500 text-white px-4 py-2 rounded shadow hover:bg-green-600" href="add-client.php" role="button">Add Client</a>
+            <h2 class="text-2xl font-semibold text-yellow-500">All Clients</h2>
+            <a class="bg-yellow-500 text-white px-4 py-2 rounded shadow hover:bg-yellow-600" href="add-client.php" role="button">Add Client</a>
         </div>
 
         <!---------- Table Section ----------->
+
         <div class="bg-white shadow-md rounded-lg overflow-hidden">
 
             <table class="table-auto w-full border-collapse">
-
                 <thead class="bg-gray-200 text-gray-700">
                     <tr>
                         <th class="px-4 py-2 border">ID Client</th>
@@ -76,6 +92,5 @@
 
         </div>
     </div>
-
 </body>
 </html>
